6805b5e3233197c2065e31d2dc8e1b935b4c8346
Anjali havanur <78612291+Anjalih11@users.noreply.github.com>
2025-09-17 14:22:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -153,6 +153,8 @@ app.get("/food-facts", (req, res) => {
 
 
 // Start server
-app.listen(3000, () => {
-    console.log('🚀 Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
